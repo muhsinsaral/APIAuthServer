1ef6bd599acfd494bf7e4d4f3e7b5d8199fcb56d
muhsinsaral <muhsinsrl@gmail.com>
2023-07-06 23:20:56 +0300

    OptionsPattern

---
+++ SharedLibrary/Configurations/CustomTokenOption.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Configurations
+{
+    public class CustomTokenOption
+    {
+        public required List<string> Audiences { get; set; }
+        public required string Issuer { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+        public required string SecurityKey { get; set; } 
+    }
+}
--- UdemyAuthServer.API/Program.cs
+++ UdemyAuthServer.API/Program.cs
@@ -1,7 +1,9 @@
+using SharedLibrary.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,7 +17,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -23,3 +24,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
