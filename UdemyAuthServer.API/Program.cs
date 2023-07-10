0b1ef70bffcf78b06c47616dd0891b05a6f8bb28
muhsinsaral <muhsinsrl@gmail.com>
2023-07-10 08:18:54 +0300

    startup

--- UdemyAuthServer.API/Program.cs
+++ UdemyAuthServer.API/Program.cs
@@ -1,11 +1,68 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using SharedLibrary.Configurations;
 using UdemyAuthServer.Core.Configuration;
+using UdemyAuthServer.Core.Models;
+using UdemyAuthServer.Core.Repositories;
+using UdemyAuthServer.Core.Services.Abstract;
+using UdemyAuthServer.Core.UnitOfWork;
+using UdemyAuthServer.Data;
+using UdemyAuthServer.Data.Repositories;
+using UdemyAuthServer.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//DI Register
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped(typeof(IGenericRepository<>) , typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IServiceGeneric<,>) , typeof(ServiceGeneric<,>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("SqlServer"), 
+        sqlOptions =>
+        {
+            sqlOptions.MigrationsAssembly("UdemyAuthServer.Data");
+        });
+});
+
+builder.Services.AddIdentity<UserApp, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequireNonAlphanumeric = false;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+//DI Register
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
 builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = tokenOptions.Issuer,
+        ValidAudience = tokenOptions.Audience,
+        IssuerSigningKey = SignService.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+        ValidateLifetime = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
